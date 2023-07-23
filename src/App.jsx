72f126c08029c88d81cc7ad5bccd03f06a2f4bcf
Benedicte <101096042+bendiz@users.noreply.github.com>
2023-07-23 22:22:02 +0200

    Remove import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import Terminal from "./components/Terminal.jsx";
 import Projects from "./components/Projects";
-import Project from "./components/Project.jsx";
 
 export default function App() {
   return (
